298387d23af6cccc1924bdc167b02231c0ec812d
Nabeel Ahmed <naabeelahhmed@gmail.com>
2023-05-21 03:05:46 +0530

    debug

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://ai-img-generator-mnqx.onrender.com/api/v1/post', {
+      const response = await fetch('https://ai-img-generator-8x8y.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
